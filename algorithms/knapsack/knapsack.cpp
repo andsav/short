9d96320d88a8833740df85f42b5a857f9fe4c853
Andrei Savin (MacBook) <andrei@andreisavin.com>
2017-06-22 01:46:02 -0400

    Knapsack

--- algorithms/knapsack/knapsack.cpp
+++ algorithms/knapsack/knapsack.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-#define FOR_0N for(i=0; i<N; ++i)
-#define FOR_1N for(i=1; i<N; ++i)
-#define FOR_0W for(j=0; j<W+1; ++j)
-#define FOR_N0 for(i=N-1; i>=0; --i)
+#define FOR_0N for(i=0; i<=N; ++i)
+#define FOR_1N for(i=1; i<=N; ++i)
+#define FOR_0W for(j=0; j<=W; ++j)
+#define FOR_N0 for(i=N; i>=0; --i)
 
 using namespace std;
 
@@ -15,40 +15,48 @@ int main() {
     /*
      * INIT
      */
-    picks = new int[N];
-    s = new int[N];
-    w = new int[N];
-    memo = new int *[N];
-    pick = new bool *[N];
-
+    picks = new int[N + 1];
+    s = new int[N + 1];
+    w = new int[N + 1];
+    memo = new int *[N + 1];
+    pick = new bool *[N + 1];
     FOR_0N {
+        memo[i] = new int[W + 1];
+        pick[i] = new bool[W + 1];
+    };
+
+    w[0] = s[0] = 0;
+    FOR_1N {
         cin >> wi >> si;
         w[i] = wi;
         s[i] = si;
-
-        memo[i] = new int[W + 1];
-        pick[i] = new bool[W + 1];
     }
 
     /*
      * BUILD TABLE
      */
     FOR_0W memo[0][j] = 0;
-    FOR_1N FOR_0W memo[i][j] = (j < w[i]) ? 0 : max(s[i] + memo[i - 1][j - w[i]], memo[i - 1][j]);
-    FOR_0N FOR_0W pick[i][j] = memo[i][j] != 0 && s[i] + memo[i - 1][j - w[i]] > memo[i - 1][j];
+    FOR_1N FOR_0W memo[i][j] = (j < w[i]) ? memo[i - 1][j] : max(s[i] + memo[i - 1][j - w[i]], memo[i - 1][j]);
+    FOR_0N FOR_0W pick[i][j] = !(memo[i][j] == 0 || j < w[i] || s[i] + memo[i - 1][j - w[i]] <= memo[i - 1][j]);
 
+    /*
+    FOR_0N {
+        FOR_0W cout << memo[i][j] << "\t";
+        cout << endl;
+    }
+    */
 
     /*
      * OUTPUT
      */
 
-    cout << memo[N - 1][W];                                                         // S max weight
+    cout << memo[N][W];                                                              // S max weight
 
     j = 0;
     FOR_N0 {
-        if(W <= 0) break;
+        if (W <= 0) break;
         if (pick[i][W]) {
-            picks[j] = i + 1;
+            picks[j] = i;
             W -= w[i];
             j++;
         }
@@ -57,7 +65,7 @@ int main() {
     cout << " " << j << endl;                                                       // K # of picks
 
 
-    if(j > 0) {
+    if (j > 0) {
         cout << picks[0];
         for (i = 1; i < j; ++i) cout << " " << picks[i];                            // indexes i
         cout << endl;
@@ -73,4 +81,4 @@ int main() {
     }
     delete[] memo;
     delete[] pick;
-};
+}
