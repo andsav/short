054adb13fc725bc697ce1b052f50f4d7114da234
Andrei Savin <andrei@andreisavin.com>
2018-07-26 23:13:14 -0400

    Euler

---
+++ etc/euler/3.js
@@ -0,0 +1,16 @@
+let seen = [];
+for(let i=3; i<Math.sqrt(600851475143); i+=2) {
+    if(600851475143%i === 0) {
+        let prime = true;
+        for(let x of seen) {
+            if(i%x === 0) {
+                prime = false;
+                break;
+            }
+        }
+        if(prime) {
+            seen.push(i)
+        }
+    }
+}
+console.log(seen[seen.length-1])
